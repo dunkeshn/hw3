7c1d8618f095e602a6df329a2072f7ac47af77bb
Ivan <130477079+dunkeshn@users.noreply.github.com>
2024-10-15 23:57:17 +0500

    Add files via upload

--- core/models.py
+++ core/models.py
@@ -2,25 +2,28 @@ from django.db import models
 
 
 class Product(models.Model):
+    class Category(models.TextChoices):
+        DRINK = 'DRINK', 'Напиток'
+        DESSERT = 'DESSERT', 'Десерт'
+        HOT = 'HOT', 'Горячее блюдо'
+
+    class Availability(models.TextChoices):
+        ABSENT = 'ABSENT', 'Товар отсутствует'
+        AVAILABLE = 'AVAILABLE', 'В наличии'
+
     name = models.CharField('Название товара', max_length=255)
-    price = models.FloatField('Цена товара')
+    price = models.DecimalField(verbose_name='Цена товара', max_digits=10, decimal_places=2)
     description = models.TextField('Описание товара', null=True, blank=True, )
-    category = models.CharField('Категория товара', max_length=100, choices=[
-        ('DRINK', 'Напиток'),
-        ('DESSERT', 'Десерт'),
-        ('HOT', 'Горячее блюдо'),
-    ], null=True)
-    availability = models.CharField('Наличие', max_length=100, choices=[
-        ('ABSENT', 'Товар отсутствует'),
-        ('AVAILABLE', 'В наличии'),
-    ], default='AVAILABLE')
+    category = models.CharField('Категория товара', max_length=100, choices=Category.choices, null=True)
+    availability = models.CharField('Наличие', max_length=100, choices=Availability.choices,
+                                    default=Availability.AVAILABLE)
     image = models.ImageField('Фотография товара', blank=True)
     create_date = models.DateField('Дата создания товара', auto_now_add=True)
 
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
-        ordering = ('create_date', )
+        ordering = ('create_date',)
 
     def __str__(self):
         return self.name
@@ -34,68 +37,86 @@ class Cafe(models.Model):
     class Meta:
         verbose_name = 'Кофейня'
         verbose_name_plural = 'Кофейни'
-        ordering = ('barista_number', )
+        ordering = ('barista_number',)
 
     def __str__(self):
         return self.address
 
 
 class Order(models.Model):
+    class DeliveryStatus(models.TextChoices):
+        PAYING = 'PAYING', 'Оплата заказа'
+        PREPARING = 'PREPARING', 'Заказ готовится'
+        DELIVERING = 'DELIVERING', 'Заказ доставляется'
+        RECEIVED = 'RECEIVED', 'Заказ получен'
+
+    class PaymentMethod(models.TextChoices):
+        BANK_CARD = 'BANK_CARD', 'Банковская карта'
+
     products = models.ManyToManyField(to=Product, related_name='orders',
                                       verbose_name='Товары')
     cafe = models.ForeignKey(to=Cafe, on_delete=models.PROTECT, related_name='orders', verbose_name='Кофейни',
                              null=True)
-    sum = models.FloatField('Сумма')
-    delivery_status = models.CharField('Статус доставки', max_length=100, choices=[
-        ('PAYING', 'Оплата заказа'),
-        ('PREPARING', 'Заказ готовится'),
-        ('DELIVERING', 'Заказ доставляется'),
-        ('RECEIVED', 'Заказ получен'),
-    ], default='PAYING')
-    payment_method = models.CharField('Способ оплаты', max_length=100, choices=[
-        ('BANK_CARD', 'Банковская карта'),
-    ], null=True)
+    sum = models.DecimalField(verbose_name='Сумма', max_digits=10, decimal_places=2)
+    delivery_status = models.CharField('Статус доставки', max_length=100, choices=DeliveryStatus.choices,
+                                       default=DeliveryStatus.PAYING)
+    payment_method = models.CharField('Способ оплаты', max_length=100, choices=PaymentMethod.choices, null=True)
     address = models.TextField('Адрес')
     waiting_time = models.TimeField('Время доставки')
 
     class Meta:
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
-        ordering = ('waiting_time', )
+        ordering = ('waiting_time',)
 
     def __str__(self):
         return self.address
 
 
 class Courier(models.Model):
+    class CourierCategory(models.TextChoices):
+        FOOT = 'FOOT', 'Пеший курьер'
+        BICYCLE = 'BICYCLE', 'Велокурьер'
+        CAR = 'CAR', 'Автокурьер'
+
+    class CourierStatus(models.TextChoices):
+        AT_WORK = 'AT_WORK', 'На работе'
+        RELAXING = 'RELAXING', 'Отдыхает'
+        BREAK = 'BREAK', 'Перерыв'
+
     name = models.CharField('Имя', max_length=255)
-    courier_category = models.CharField('Категория курьера', max_length=100, choices=[
-        ('FOOT', 'Пеший курьер'),
-        ('BICYCLE', 'Велокурьер'),
-        ('CAR', 'Автокурьер'),
-    ], default='CAR')
-    courier_status = models.CharField('Категория курьера', max_length=100, choices=[
-        ('AT_WORK', 'На работе'),
-        ('RELAXING', 'Отдыхает'),
-        ('BREAK', 'Перерыв'),
-    ])
+    courier_category = models.CharField(
+        'Категория курьера',
+        max_length=100,
+        choices=CourierCategory.choices,
+        default=CourierCategory.CAR
+    )
+    courier_status = models.CharField(
+        'Статус курьера',
+        max_length=100,
+        choices=CourierStatus.choices
+    )
 
     class Meta:
         verbose_name = 'Курьер'
         verbose_name_plural = 'Курьеры'
-        ordering = ('courier_status', )
+        ordering = ('courier_status',)
 
     def __str__(self):
         return self.name
 
+
 class Cart(models.Model):
     products = models.ManyToManyField(to=Product, related_name='carts', verbose_name='Товары')
-    sum = models.FloatField('Сумма')
+    sum = models.DecimalField(verbose_name='Сумма', max_digits=10, decimal_places=2)
 
     class Meta:
         verbose_name = 'Корзина'
         verbose_name_plural = 'Корзины'
-        ordering = ('sum', )
+        ordering = ('sum',)
+
+    def __str__(self):
+        return f'Корзина {self.id} на сумму {self.sum}'
 
 class DeliveryDescription(models.Model):
     description = models.TextField('Описание доставки', null=True, blank=True)
@@ -103,7 +124,7 @@ class DeliveryDescription(models.Model):
     class Meta:
         verbose_name = 'Описание'
         verbose_name_plural = 'Описания'
-        ordering = ('id', )
+        ordering = ('id',)
 
     def __str__(self):
         return 'Описание'
@@ -119,14 +140,7 @@ class Delivery(models.Model):
     class Meta:
         verbose_name = 'Доставка'
         verbose_name_plural = 'Доставки'
-        ordering = ('time_left', )
+        ordering = ('time_left',)
 
     def __str__(self):
         return f'Доставка #{self.id}'
-
-
-
-
-
-
-
