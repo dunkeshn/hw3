e04a8f4282cf7054a51231a4dd1abc89cfeb85e0
Ivan <130477079+dunkeshn@users.noreply.github.com>
2024-10-17 11:02:49 +0500

    Add files via upload

--- core/views.py
+++ core/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Order
 
-# Create your views here.
+
+class OrderListView(ListView):
+    model = Order
+    template_name = 'orders/order_list.html'
+    context_object_name = 'orders'
+
+
+class OrderDetailView(DetailView):
+    model = Order
+    template_name = 'orders/order_detail.html'
+    context_object_name = 'order'
+
+
+class OrderCreateView(CreateView):
+    model = Order
+    template_name = 'orders/order_form.html'
+    fields = ['products', 'cafe', 'sum', 'delivery_status', 'payment_method', 'address', 'waiting_time']
+    def get_success_url(self):
+        return reverse_lazy('order_list')
+
+
+class OrderUpdateView(UpdateView):
+    model = Order
+    template_name = 'orders/order_form.html'
+    fields = ['products', 'cafe', 'sum', 'delivery_status', 'payment_method', 'address', 'waiting_time']
+    def get_success_url(self):
+        return reverse_lazy('order_list')
+
+
+class OrderDeleteView(DeleteView):
+    model = Order
+    template_name = 'orders/order_confirm_delete.html'
+    success_url = reverse_lazy('order_list')
